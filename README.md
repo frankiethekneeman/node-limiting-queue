33106cd6f491ae7d31b9f890b1ae90e7f36bd300
Francis J.. Van Wetering IV <francis.wetering@complex.com>
2014-11-27 11:52:39 -0500

    Documentation, ALL DAY.

--- README.md
+++ README.md
@@ -4,71 +4,137 @@
 
     var LimitingQueue = require('limiting-queue');
 
-    var queue = new LimitingQueue({
-        /**
-         *  The maximum numbers of simultaneous workers.  An integer greater than 
-         *  zero.  Any negative number will be interpreted as "no limit."
-         *  
-         *  Defaults to -1.
-         */
-        maxWorkers: -1
-        /**
-         *  The maximum number of retries for a queue entry.  An integer greater than 
-         *  zero.  Any negative number will be interpreted as "no limit."
-         *  
-         *  Defaults to -1.
-         */
-        , maxRetries: -1
-        /**
-         *  The number of milliseconds to wait before declaring a job failed by 
-         *  timeout.  An integer greater than zero.  Any negative number will be 
-         *  interpreted as "no limit."
-         *  
-         *  Defaults to -1.
-         */
-        , maxWait: -1
-        /**
-         *  The function to call to do work on a queue entry.
-         *  
-         *  Defaults to an empty function.
-         *  
-         *  @param payload The information previously passed into the "push" 
-         *      function.
-         *  @param previousAttempts the number of times this payload has previously 
-         *      been pushed to this function.
-         *  @param deferred The deferred object to declare success or failure on the 
-         *      queue item.
-         *  
-         *  @return (Optional) a function to be called in case of a timeout (to do 
-         *      any necessary cleanup).
-         */
-        , callback: function(payload, previousAttempts, deferred){}
-        /**
-         *  The function to call when a queue entry flunks out of the queue entirely.
-         *  
-         *  Defaults to an empty function.
-         *  
-         *  @param payload The information previously passed into the "push" 
-         *      function.
-         *  @param totalAttempts the total number of attempts made to process this 
-         *      information.
-         *  @param errors The list of errors that caused this to error out.
-         */
-        , failure: function(payload, totalAttempts, errors){}
-        /**
-         *  A progress callback to be called every time a new worker starts.
-         *  
-         *  Defaults to an empty function.
-         *  
-         *  @param queueSize The number of jobs in the queue.
-         *  @param workers The number of workers currently working.
-         */
-        , progress: function(queueSize, workers){}
-    });
- 
+    var queue = new LimitingQueue(options);
+
+### Options
+
+All of these options can be modified after intitalization by setting them ine the `queue.opts` object.
+
+So, for instance:
+
+    queue.opts.maxWorkers = 3;
+
+or, to reset an option back to its default:
+
+    delete opts.maxWorkers;
+
+#### maxWorkers
+        
+The maximum numbers of simultaneous workers.  An integer greater than 
+zero.  Any negative number will be interpreted as "no limit."
+Defaults to -1.
+        
+#### maxRetries
+
+The maximum number of retries for a queue entry.  An integer greater than 
+zero.  Any negative number will be interpreted as "no limit."
+Defaults to -1.
+
+#### maxWait
+
+The number of milliseconds to wait before declaring a job failed by 
+timeout.  An integer greater than zero.  Any negative number will be 
+interpreted as "no limit."
+Defaults to -1.
+
+#### maxQueueSize
+
+The maximum allowed number of items waiting in the queue.  Integer greater than zero.  Any negative number will
+be interpreted as no limit.
+Defaults to -1.
+
+#### callback
+
+The function to call to do work on a queue entry.
+May optionally return a function to be called in case of a timeout (to do any necessary cleanup).
+Defaults to a function which trivially declares the object complete.
+
+Takes three parameters:
+
+##### payload
+
+The information previously passed into the "push" function.
+
+##### previousAttempts 
+
+The number of times this payload has previously been pushed to this function.
+
+##### deferred 
+The deferred object to declare success or failure on the queue item.  Call `deferred.fulfill()` to
+declare success, and `deferred.reject(error)` to declare a failure.
+
+### failure.
+
+The function to call when a queue entry flunks out of the queue entirely.  
+Defaults to an empty function.
+
+Takes three parameters:
+
+##### payload 
+
+The information previously passed into the "push" function.
+
+##### totalAttempts 
+
+The total number of attempts made to process this information.
+
+##### errors 
+
+The array of errors that caused this to error out - in order.
+        
+#### progress
+
+A progress callback to be called every time a new worker starts.
+
+Takes two paramters:
+
+##### queueSize
+
+The number of jobs in the queue. (Not being worked on.)
+
+##### workers 
+
+The number of workers currently working.
+
+##### autoStart
+
+Pass true to start the queue immediately, false to let it wait.
+    
+##### retryImmediately
+    
+Pass true to push failed jobs to the head of the queue, false to push them to the back.
+
+### queue.push(payload)
+
+Push a new job to the front of the queue.
+Returns true if the job is added, false otherwise.
+
+### queue.append(payload)
+
+Append to the back of the queue.
+Returns true if the job is added, false otherwise.
+
+### queue.size()
+
+Returns the number of objects waiting in the queue.
+
+### queue.workers()
+
+Returns the number of active workers.
+
+### queue.start()
+
+Start the queue.
+
+### queue.stop()
+
+Stop the queue.  This does not cancel working workers - it just ceases the creation of new workers.
 
 ## Contributing
 
 Please maintain the existing style - but do open a pull request if you have a bugfix or a cool feature.
 Be sure to document your change.  Pull requests are preferred to bug reports (though if you submit a bug
 report, you're welcome to fix your own bug).
+
+Please be sure to update all documentation to reflect your changes - add to the Readme files and the in
+code commenting.
--- limiting-queue.js
+++ limiting-queue.js
@@ -5,22 +5,32 @@ var q = require('q')
  *  Here is where I will document the default values for the various arguments to be passed into this constructor.
  */
 var defaults = {
+
     /**
      *  The maximum numbers of simultaneous workers.  An integer greater than zero.  Any negative number will
      *  be interpreted as "no limit."
      */
     maxWorkers: -1
+
     /**
      *  The maximum number of retries for a queue entry.  An integer greater than zero.  Any negative number will be 
      *  interpreted as "no limit."
      */
     , maxRetries: -1
+
     /**
      *  The number of milliseconds to wait before declaring a job failed by timeout.  
      *  An integer greater than zero.  Any negative number will be 
      *  interpreted as "no limit."
      */
     , maxWait: -1
+
+    /**
+     *  The maximum allowed number of items waiting in the queue.  Integer greater than zero.  Any negative number will
+     *  be interpreted as no limit.
+     */
+    , maxQueueSize: -1
+
     /**
      *  The function to call to do work on a queue entry.
      *  
@@ -30,7 +40,8 @@ var defaults = {
      *  
      *  @return (Optional) a function to be called in case of a timeout (to do any necessary cleanup).
      */
-    , callback: function(payload, previousAttempts, deferred){}
+    , callback: function(payload, previousAttempts, deferred){deferred.fulfill()}
+
     /**
      *  The function to call when a queue entry flunks out of the queue entirely.
      *  
@@ -39,6 +50,7 @@ var defaults = {
      *  @param errors The list of errors that caused this to error out.
      */
     , failure: function(payload, totalAttempts, errors){}
+
     /**
      *  A progress callback to be called every time a new worker starts.
      *  
@@ -46,111 +58,239 @@ var defaults = {
      *  @param workers - The number of workers currently working.
      */
     , progress: function(queueSize, workers){}
+
+    /**
+     *  Pass true to start the queue immediately, false to let it wait.
+     */
+    , autoStart: true
+
+    /**
+     *  Pass true to push failed jobs to the head of the queue, false to push them to the back.
+     */
+    , retryImmediately: false
 }
 
 module.exports = function LimitingQueue(opts) {
     opts.prototype = defaults;
+    this.opts = opts;
     var queueHead = null
         , queueTail = null
         , queueSize = 0
         , workers = 0
+        , working = this.opts.autoStart
         ;
+
+    /**
+     *  Consume queue Items... This internal function fills the available worker space with 
+     *  workers (so long as there are queue items still waiting to be processes) and calls
+     *  the Progress callback.
+     */
     function consume(){
-        var startWorkers = workers;
-        for (; opts.maxWorkers < 0 || workers < opts.maxWorkers;) {
+        while(working && (this.opts.maxWorkers < 0 || workers < this.opts.maxWorkers)) {
             if (!(function() {
                 var toWork = queueHead;
                 if (!toWork) {
                     return false;
-                }
+                }//if not toWork
+
                 queueSize--;
                 workers++;
                 queueHead = queueHead.next;
                 if (!queueHead) {
                     queueTail = null;
-                }
+                }//if not queueHead
+
                 //This is the deferred that we'll pass into the worker function.
                 var deferred = q.defer();
                 //This is the timeout for expiration.
                 var waitItOut = false;
                 try {
+
                     /**
                      *  If the queue function returns a function, we'll call it on expiration.
                      */
-                    var timeoutCallback = opts.callback(toWork.payload, toWork.retries, deferred);
+                    var timeoutCallback = this.opts.callback(toWork.payload, toWork.retries, deferred);
+
                     /**
                      *  If there's a maximum execution time, set a callback.
                      */
-                    if (opts.maxWait < 0) {
+                    if (this.opts.maxWait >= 0) {
                         waitItOut = setTimeout(function() {
                             if (typeof timeoutCallback == 'function') {
                                 timeoutCallback();
-                            }
+                            }//if Callback
                             deferred.reject(new Error('Maximum Time'));
-                        }, opts.maxWait);
-                    }
-                } catch (err) {
+                        }, this.opts.maxWait);
+                    }//if time limit.
+                } catch (err) {//Try
                     deferred.reject(err);
-                }
-                deferred.promise.then(function(content) { //Success!
+                }//Catch Err
+                deferred.promise.then(function(result) {
+
+                    /**
+                     *  ON success, note worker completion, clear any existing timeout,
+                     *  and cycle back around on the queue.
+                     */
                     workers--;
+                    if (waitItOut !== false)
+                        clearTimeout(waitItOut);
                     consume();
-                    clearTimeout(waitItOut);
                 }, function(error) {
+
+                    /**
+                     *  On failure, note worker completion.  Add the error to the error
+                     *  array.  If there are still retries left, replace it at the back of the
+                     *  queue, If there are not - replace the item in the queue. Then loop back.
+                     */
                     workers--;
                     try {
                         toWork.errors.push(error);
-                        if (opts.maxRetries < 0 || toWork.retries < opts.maxRetries) {
+                        if (this.opts.maxRetries < 0 || toWork.retries < this.opts.maxRetries) {
                             toWork.retries++;
-                            pushToQueue(toWork);
-                        } else {
-                            opts.failure(toWork.payload, toWork.retries + 1, toWork.errors);
-                        }
-                        consume();
+                            if (this.opts.retryImmediately) {
+                                privatePush(toWork);
+                            } else {//If retry Immediately
+                                privateAppend(toWork);
+                            }//else
+                        } else {//If should retry
+                            this.opts.failure(toWork.payload, toWork.retries + 1, toWork.errors);
+                        }//If total failure
                         if (waitItOut !== false) {
                             clearTimeout(waitItOut);
-                        }
-                    } catch (e) {
+                        }//If there's a timeout.
+                        consume();
+                    } catch (e) {//Try
                         console.log(e.stack);
-                    }
+                    } //Catch Error
                 });
-                //Prep the timeout for time limiting.
                 return true;
             })()) {
                 break;
-            }
-        }
-        opts.progress(queueSize, workers);
-        if (workers == 0 && queueSize > 0) {
+            }//if there are no more workers
+        }//while there is still worker space
+
+        this.opts.progress(queueSize, workers);
+
+        //If there are no workers, but still items in the queue (and we're supposed to be working)
+        //Recurse back.
+        if (working && workers == 0 && queueSize > 0) {
             consume();
-        }
-    }
-    function pushToQueue(newWork) {
+        }//If should retry
+    }//consume()
+
+    /**
+     *  Push to the front of the queue.  Private function.
+     *  
+     *  @param newWork a queue item to be added to the queue
+     */
+    function privatePush(newWork) {
+        //If there is a head.
+        if (queueHead) {
+            newWork.next = queueHead;
+            queueHead = newWork;
+        } else { //If there's a head
+            //If there's no head, there's no tail.
+            queueTail = queueHead = newWork;
+        }//else, no head
+        queueSize++;
+    }//privatePush(newWork)
+
+    /**
+     *  Append to the back of the queue.  Private function.
+     *  
+     *  @param newWork a queue item to be added to the queue
+     */
+    function privateAppend(newWork) {
         //If there is a tail.
         if (queueTail) {
             queueTail = queueTail.next = newWork;
-        } else {
+        } else {//If there's a tail
             //If there's no tail, there's no head.
             queueTail = queueHead = newWork;
-        }
+        }//If there's no tail
         queueSize++;
-    }
-    this.push = function(payload) {
+    }//privatePush(newWork)
+
+    /**
+     *  Generalized private function to abstract queue node generation
+     *  and adding.
+     *  
+     *  @param payload the payload to be (eventually) delivered back to the
+     *    workers.
+     *  @param addAction a callback which takes a queue node and adds it to the queue.
+     *  
+     *  @return true if the payload is added, false otherwise.
+     */
+    function addPayload(payload, addAction) {
         var newWork = {
             payload: payload
             , retries: 0
             , next: null
             , errors: []
-        }
+        };//newWork Object
 
-        pushToQueue(newWork);
+        var toReturn = false;
+        if (queueSize < this.opts.maxQueueSize || this.opts.maxQueueSize < 0) {
+            addAction(newWork);
+            toReturn = true;
+        }//If there's room in the queue.
         //Make sure to call consume in case there are no workers running.
         consume();
-    }
-    this.inQueue = function() {
+        return toReturn;
+    }//addPayload(payload, addAction)
+
+    /**
+     *  Push to the front of the queue.
+     *  
+     *  @param payload The data to eventually be delivered back.
+     *  
+     *  @return true if the payload is added, false otherwise.
+     */
+    this.push = function(payload) {
+        return addPayload(payload, privatePush);
+    }//push(payload)
+
+    /**
+     *  Append to the back of the queue.
+     *  
+     *  @param payload The data to eventually be delivered back.
+     *  
+     *  @return true if the payload is added, false otherwise.
+     */
+    this.append = function(payload) {
+        return addPayload(payload, privateAppend);
+    }//append(payload)
+
+    /**
+     *  Check the size of the queue.
+     *  
+     *  @return the number of objects waiting in the queue.
+     */
+    this.size = function() {
         return queueSize;
-    }
+    }//size()
+
+    /**
+     *  Check how many workers are currently doing work.
+     *  
+     *  @return the number of active workers.
+     */
     this.workers = function() {
         return workers;
-    }
+    }//workers()
+
+    /**
+     *  Start the queue.
+     */
+    this.start = function() {
+        working = true;
+        consume();
+    }//start()
+
+    /**
+     *  Stop the queue.  This does not cancel working workers - it just ceases the creation of new workers.
+     */
+    this.stop = function() {
+        working = false;
+    }//stop()
 }
