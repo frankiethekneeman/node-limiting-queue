054dae8c2883bd3ab3cba3a541f9072ec0823ff0
Francis J.. Van Wetering IV <francis.wetering@complex.com>
2015-02-13 17:30:54 -0500

    Direct Override of the Object Prototype is verbotten.  Copy it by hand.

--- src/utils.js
+++ src/utils.js
@@ -1,16 +1,32 @@
-module.exports = {
-    objectCopy: function(src, deep) {
-        var dest = {};
-        if (typeof src !== 'object') {
-            return src;
-        }
-        Object.keys(src).forEach(function(key) {
-            if (!deep || typeof src !== 'object') {
-                dest[key] = src[key];
-            } else {
-                dest[key] = this.objectCopy(src[key], deep);
-            }
-        }.bind(this));
-        return dest;
+var defaults = require('./defaults');
+function objectCopy(src, deep) {
+    var dest = {};
+    if (typeof src !== 'object') {
+        return src;
     }
+    Object.keys(src).forEach(function(key) {
+        if (!deep || typeof src !== 'object') {
+            dest[key] = src[key];
+        } else {
+            dest[key] = objectCopy(src[key], deep);
+        }
+    }.bind(this));
+    return dest;
+}
+
+function Options = function(src) {
+    Object.keys(src).forEach(function(key) {
+        if (!deep || typeof src !== 'object') {
+            dest[key] = src[key];
+        } else {
+            dest[key] = objectCopy(src[key], deep);
+        }
+    };
+}
+
+Options.prototype = defaults;
+
+module.exports = {
+    objectCopy: objectCopy
+    , Options: Options
 };
