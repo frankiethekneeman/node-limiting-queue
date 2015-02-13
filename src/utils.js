9712a912cb0a7db2bfa5d55f4e08f9a4437e90df
Francis J.. Van Wetering IV <francis.wetering@complex.com>
2015-02-13 18:00:14 -0500

    Fixing Typos to utilize Travis.

--- src/utils.js
+++ src/utils.js
@@ -12,17 +12,17 @@ function objectCopy(src, deep) {
         }
     }.bind(this));
     return dest;
-}
+};
 
-function Options = function(src) {
+function Options(src) {
     Object.keys(src).forEach(function(key) {
-        if (!deep || typeof src !== 'object') {
-            dest[key] = src[key];
+        if (typeof src !== 'object') {
+            this[key] = src[key];
         } else {
-            dest[key] = objectCopy(src[key], deep);
+            this[key] = objectCopy(src[key]);
         }
-    };
-}
+    }.bind(this));
+};
 
 Options.prototype = defaults;
 
